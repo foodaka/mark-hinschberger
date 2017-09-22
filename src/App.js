9bbf1cf9c5bd7402379a721fcd535774dea37799
markhinschberger <markhinschberger@gmail.com>
2017-09-22 22:39:23 +0800

    polish

--- src/App.js
+++ src/App.js
@@ -4,61 +4,56 @@ import "./App.css";
 import Mark_Hinschberger_PM from "./Mark_Hinschberger_PM.pdf";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div>
-                    <p>
-                        Hi, I'm Mark Hinschberger and I like to write code and
-                        build cool products <i className="em em-sunglasses"></i>.
-                    </p>
-                    <p>
-                        I work with Javascript and use React/Redux to build UI.
-                    </p>
-                    <p>
-                        I just launched a new product to market called&nbsp;
-                        <a
-                            className="notice"
-                            href={"http://www.wordup.com.tw/about"}
-                        >
-                            WordUp
-                        </a>. You should&nbsp;
-                        <a
-                            className="notice"
-                            href={"http://www.wordup.com.tw/about"}
-                        >
-                            definitly
-                        </a> check it out.
-                    </p>
-                    <p>
-                        <a
-                            className="notice"
-                            href="mailto:markhinschberger@gmail.com"
-                        >
-                            <span className="do-it">You Should Email me&nbsp;</span>
-                        </a>
-                        if you're in need of a product manager or front end
-                        developer
-                    </p>
-                    <p>
-                        You can download my resume{" "}
-                        <a
-                            className="notice"
-                            href={Mark_Hinschberger_PM}
-                            download="download"
-                        >
-                            here.
-                        </a>
-                    </p>
-                    <p>
-                        <a className='cool-icons' ><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-                        <a className='cool-icons' href='https://www.github.com/foodaka'><i className="fa fa-github" aria-hidden="true"></i></a>
-                        <a className='cool-icons'><i className="fa fa-envelope" aria-hidden="true"></i></a>
-                    </p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <p>
+            Hi, I'm Mark Hinschberger and I like to write code and build cool
+            products <i className="em em-sunglasses" />.
+          </p>
+          <p>I work with Javascript and use React/Redux to build UI.</p>
+          <p>
+            I just launched a new product to market called&nbsp;
+            <a className="notice do-it" href={"http://www.wordup.com.tw/about"}>
+              WordUp
+            </a>. You should&nbsp;
+            <a className="notice do-it" href={"http://www.wordup.com.tw/about"}>
+              definitly
+            </a>{" "}
+            check it out.
+          </p>
+          <p>
+            <a className="notice" href="mailto:markhinschberger@gmail.com">
+              <span className="do-it">You Should Email me&nbsp;</span>
+            </a>
+            if you're in need of a product manager or front end developer
+          </p>
+          <p>
+            You can download my resume{" "}
+            <a
+              className="notice"
+              href={Mark_Hinschberger_PM}
+              download="download"
+            >
+              <span className='do-it'>here</span>.
+            </a>
+          </p>
+          <p>
+            <a className="cool-icons" href='https://www.linkedin.com/in/mark-hinschberger-3370962b/'>
+              <i className="fa fa-linkedin-square fa-lg" aria-hidden="true" />
+            </a>
+            <a className="cool-icons fa-lg" href="https://www.github.com/foodaka">
+              <i className="fa fa-github" aria-hidden="true" />
+            </a>
+            <a className="cool-icons" href="mailto:markhinschberger@gmail.com">
+              <i className="fa fa-envelope" aria-hidden="true" />
+            </a>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
